7f298c3c34606275acc6d3a494fe39b3632e9013
Isabella Salazar Ternus <60165008016@senacrs.edu.br>
2025-03-31 10:13:01 -0300

    Lampada

---
+++ script.js
@@ -0,0 +1,12 @@
+const ligar = document.getElementById('Ligar');
+const desligar = document.getElementById('Desligar');
+const Lampada = document.getElementById('Lampada');
+
+function ligarLampada() {
+lampada.src="https://copalux.cdn.magazord.com.br/img/2020/08/produto/2504/lampada-led-taschibra-filamento-color-a60-4w-bivolt-e27-amarela.png?ims=800x800";
+}
+function desligarLampada() {
+    lampada.src="https://cdn.awsli.com.br/600x1000/2606/2606450/produto/235986344210c64d020.jpg";
+}
+Ligar.addEventListener('click',LigarLampada);
+Desligar.addEventListener('click',DesligarLampada);
